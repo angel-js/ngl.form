91dcbc36e47db3e1d3702810b37f76fd886aa367
Pau Fracés <pfraces@gmail.com>
2016-02-12 05:34:36 +0100

    replaced link with controller

--- src/form.js
+++ src/form.js
@@ -11,48 +11,48 @@ angular.module('ngl.form', [])
   var $parse = $injector.get('$parse');
   var NGL_KEYCODE = $injector.get('NGL_KEYCODE');
 
-  var link = function (scope, element, attrs) {
+  var compile = function ($scope, $element, $attrs) {
     var submit = function () {
-      scope.$apply(function () {
-        $parse(attrs.nglFormSubmit)(scope);
+      $scope.$apply(function () {
+        $parse($attrs.nglFormSubmit)($scope);
       });
     };
 
     // Pressing enter in a text field
-    angular.element(element[0].querySelectorAll('input,textarea'))
+    angular.element($element[0].querySelectorAll('input,textarea'))
     .on('keyup', function (event) {
       if (event.keyCode === NGL_KEYCODE.ENTER) { submit(); }
     });
 
     // Pressing space or enter in an input or button with type=submit
-    angular.element(element[0].querySelectorAll('[type="submit"]'))
+    angular.element($element[0].querySelectorAll('[type="submit"]'))
     .on('keyup', function (event) {
       if (event.keyCode === NGL_KEYCODE.SPACE) { submit(); }
       if (event.keyCode === NGL_KEYCODE.ENTER) { submit(); }
     });
 
     // Clicking a button or input with type=submit
-    angular.element(element[0].querySelectorAll('[type="submit"]'))
+    angular.element($element[0].querySelectorAll('[type="submit"]'))
     .on('click', submit);
   };
 
   return {
     scope: true,
-    link: link
+    compile: compile
   };
 })
 
 .directive('nglFormPassword', function () {
   'use strict';
 
-  var link = function (scope, element, attrs) {
-    element.on('focus', function () {
-      attrs.$set('type', 'password');
+  var compile = function ($element, $attrs) {
+    $element.on('focus', function () {
+      $attrs.$set('type', 'password');
     });
   };
 
   return {
     scope: true,
-    link: link
+    compile: compile
   };
 });
